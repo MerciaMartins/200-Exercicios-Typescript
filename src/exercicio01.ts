5d1ef5e567490c6c22df9ceadd3df32c3a1c2320
MerciaM <merciaessavagaesua@gmail.com>
2025-10-19 23:34:16 -0300

    Exercicio 01: Declare duas variáveis chamadas idade  e peso, atribua valores a elas e imprima os valores na console.

---
+++ src/exercicio01.ts
@@ -0,0 +1,7 @@
+
+//Declare duas variáveis chamadas "idade" e "peso", atribua valores a elas e imprima os valores na console.
+
+let idade: number = 30;
+let peso: number = 58;
+
+console.log(`Ela tem ${idade} anos e pesa ${peso} quilos.`)

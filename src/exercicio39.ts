495846559d2b9b99e00c73628377548b2627156a
MerciaM <merciaessavagaesua@gmail.com>
2025-11-07 10:20:24 -0300

    Exercício 39: Usando um laço for, crie um programa que calcule o fatorial de um número.

---
+++ src/exercicio39.ts
@@ -0,0 +1,10 @@
+// Exercício 39: Usando um laço for, crie um programa que calcule o fatorial de um número.
+
+let numero: number = 5; 
+let fatorial: number = 1;
+
+for (let i: number = 1; i <= numero; i++) {
+    fatorial *= i;
+}
+
+console.log(`O fatorial de ${numero} é ${fatorial}`);

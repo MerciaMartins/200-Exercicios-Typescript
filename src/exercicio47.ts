e9ad4a903c7e6bc9fb7ceac0db6e06c37094e1c4
MerciaM <merciaessavagaesua@gmail.com>
2025-11-10 11:51:59 -0300

    Exercício 47: Escreva uma função que aceite um número como argumento e retorne o seu quadrado.

---
+++ src/exercicio47.ts
@@ -0,0 +1,7 @@
+// Exercício 47: Escreva uma função que aceite um número como argumento e retorne o seu quadrado.
+
+function quadrado(n: number): number {
+  return n * n;
+}
+
+console.log(quadrado(5));

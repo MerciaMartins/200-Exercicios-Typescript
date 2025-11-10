e0f3bf9cc2ca998d857dc51807dd096664425709
MerciaM <merciaessavagaesua@gmail.com>
2025-11-10 12:08:25 -0300

    Exercício 50: Escreva uma função que aceite uma string como argumento e retorne a string invertida.

---
+++ src/exercicio50.ts
@@ -0,0 +1,7 @@
+// Exercício 50: Escreva uma função que aceite uma string como argumento e retorne a string invertida.
+
+function inverterString(str: string): string {
+  return str.split("").reverse().join("");
+}
+
+console.log(inverterString("TypeScript"));

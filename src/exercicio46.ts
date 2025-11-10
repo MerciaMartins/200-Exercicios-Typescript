cacdd63ac01fd7e42e8ca3f24c3662fb5ac100f9
MerciaM <merciaessavagaesua@gmail.com>
2025-11-10 11:51:08 -0300

    Exercício 46: Escreva uma função que aceite dois números como argumentos e retorne sua soma.

---
+++ src/exercicio46.ts
@@ -0,0 +1,7 @@
+// Exercício 46: Escreva uma função que aceite dois números como argumentos e retorne sua soma.
+
+function soma(a: number, b: number): number {
+  return a + b;
+}
+
+console.log(soma(5, 7));

4895deb5142d86135b57e486c60a49d5b244c504
MerciaM <merciaessavagaesua@gmail.com>
2025-11-21 22:42:48 -0300

    Exercício 63: Escreva uma função que receba um número e retorne um array com todos os números primos até aquele número.

---
+++ src/exercicio63.ts
@@ -0,0 +1,24 @@
+// Exercício 63: Escreva uma função que receba um número e retorne um array com todos os números primos até aquele número.
+
+function primosAteN(n: number): number[] {
+    const primos: number[] = [];
+
+    for (let i = 2; i <= n; i++) {
+        let primo = true;
+
+        for (let j = 2; j <= Math.sqrt(i); j++) {
+            if (i % j === 0) {
+                primo = false;
+                break;
+            }
+        }
+
+        if (primo) {
+            primos.push(i);
+        }
+    }
+
+    return primos;
+}
+
+console.log(primosAteN(30));

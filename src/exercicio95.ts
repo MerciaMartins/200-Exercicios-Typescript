4d3be4384a3f90cb97735d5df28a9e76d9aa5ba3
MerciaM <merciaessavagaesua@gmail.com>
2025-12-02 08:18:00 -0300

    Exercício 95: Dado dois arrays, retorne um novo array que é a concatenação dos dois.

---
+++ src/exercicio95.ts
@@ -0,0 +1,8 @@
+//Exercício 95: Dado dois arrays, retorne um novo array que é a concatenação dos dois.
+
+const array1: number[] = [1, 2, 3];
+const array2: number[] = [4, 5, 6];
+
+const concatenado: number[] = array1.concat(array2);
+
+console.log(concatenado); 
